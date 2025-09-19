af0ee491151bd8de142d96e1573015cf399d772f
PhamTrinhDuc <duc78240@gmail.com>
2025-09-20 00:14:09 +0700

    flink jobs

--- script/flink/fraud_detection/fraud_detection_order.py
+++ script/flink/fraud_detection/fraud_detection_order.py
@@ -1,25 +1,41 @@
-from pyflink.datastream.functions import MapFunction
+import os
+import sys 
+from dotenv import load_dotenv
+load_dotenv()
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+
+from pyflink.datastream.functions import MapFunction, ProcessWindowFunction
 from pyflink.common.typeinfo import Types
-from pyflink.common.watermark_strategy import WatermarkStrategy
+from pyflink.common.watermark_strategy import WatermarkStrategy, TimestampAssigner
 from pyflink.common import Duration
 from pyflink.datastream.window import TumblingEventTimeWindows
 from pyflink.common.time import Time
+from pyflink.datastream.connectors.file_system import FileSink
+from pyflink.common.serialization import Encoder
 import json
 import os
 from backend.models.order import Order
 from utils.sender_alert import SendAlertToWebhook
 from utils.create_stream import create_kafka_source
 
-
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")
 
+# Custom TimestampAssigner class
+class OrderTimestampAssigner(TimestampAssigner):
+  def extract_timestamp(self, element, record_timestamp):
+    return element["timestamp"]
+  
 # --- Parse Order JSON ---
 class ParseOrder(MapFunction):
   def map(self, value):
-    data = json.loads(value)
-    # Chuyển order_date (giây) → mili giây cho Flink
-    data["event_time"] = int(data["order_date"]) * 1000
-    return data
+    try:
+      data = json.loads(value)
+      # Chuyển order_date (giây) → mili giây cho Flink
+      data["timestamp"] = int(data["order_date"]) * 1000
+      return data
+    except:
+      return None
 
 class FraudAlert:
   def __init__(self, customer_id, reason, value=None):
@@ -30,67 +46,79 @@ class FraudAlert:
   def __str__(self):
     extra = f" (Value: {self.value})" if self.value else ""
     return f"🚨 Fraud Alert: Customer {self.customer_id} | {self.reason}{extra}"
-  
-def fraud_detection_order(): 
-  env, raw_stream = create_kafka_source(
-    topic="orders", 
-    group_id="fraud-detection-order-group", 
-    source_name="Kafka Source - Orders")
 
-  # Parse JSON → Order object
-  order_stream = raw_stream.map(ParseOrder(), output_type=Types.PICKLED_BYTE_ARRAY())
-  # Gán timestamp & watermark
-  order_stream = order_stream.assign_timestamps_and_watermarks(
-    watermark_strategy=WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(5)), 
-    timestamp_assigner=lambda order, ts: order.timestamp  # Sử dụng timestamp từ dữ liệu
+# Process function for frequent orders
+class FrequentOrderProcessor(ProcessWindowFunction):
+  def process(self, key, context, elements, out):
+    orders = list(elements)
+    order_count = len(orders)
+    
+    if order_count > 5:
+      alert = FraudAlert(key, "Frequent orders", f"{order_count} orders in 1 minute")
+      out.collect(alert)
+  
+def fraud_detection_order(topic: str, group_id: str, src_name: str): 
+  env, raw_stream = create_kafka_source(
+    topic=topic, 
+    group_id=group_id, 
+    source_name=src_name
   )
 
+  file_sink = (
+      FileSink.for_row_format("/tmp/flink_output", Encoder.simple_string_encoder())
+      .build()
+  )
+
+  # Parse JSON → Dict object
+  parsed_stream = (raw_stream.map(ParseOrder(), output_type=Types.PICKLED_BYTE_ARRAY())
+    .filter(lambda order: order is not None)
+  )
+
+
+  order_stream = parsed_stream.assign_timestamps_and_watermarks(
+    WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(5))
+    .with_timestamp_assigner(OrderTimestampAssigner())
+  )
+
+  
   # ------- Rule 1: đơn hàng > 10 triệu ----------
   high_values_alert = order_stream.filter(
-     lambda order: order.total_price > 10000000
+     lambda order: order["total_price"] > 10000000
   ).map(
-    lambda order: FraudAlert(order.customer_id, "High value order", f"{order.total_price:,} VND"),
+    lambda order: FraudAlert(order["customer_id"], "High value order", f"{order['total_price']:,} VND"),
     output_type=Types.PICKLED_BYTE_ARRAY()
   )
 
+
   # ------- Rule 2: cảnh báo nếu số lượng > 20 ----------
   large_quantity_alert = order_stream.filter(
-     lambda order: order.quantity > 20 
+     lambda order: order["quantity"] > 20 
   ).map(
-    lambda order: FraudAlert(order.customer_id, "Large quantity order", f"{order.quantity} items"),
+    lambda order: FraudAlert(order["customer_id"], "Large quantity order", f"{order['quantity']} items"),
     output_type=Types.PICKLED_BYTE_ARRAY()
   )
 
   # -------- Rule 3: Cảnh báo nếu > 5 đơn trong 1 phút (theo customer) ----------
-  frequent_order_alert = order_stream.key_by(
-     lambda order: order.customer_id, 
-  ).windown(TumblingEventTimeWindows.of(Time.minutes(1))) \
-  .reduce(
-     # Gộp số lượng đơn hàng trong cửa sổ 
-     lambda o1, o2: Order(
-        order_id=o1.order_id, 
-        customer_id=o1.customer_id, 
-        total_price=o1.total_price + o2.total_price, 
-        quantity=o1.quantity + o2.quantity, 
-        timestamp=max(o1.timestamp, o2.timestamp)
-     ), 
-     lambda key, window, agg_order, out : ( 
-        out.collect(FraudAlert(key, "Frequent orders", f"{agg_order.quantity} orders in 1 minute"))
-        if agg_order.quantity > 5 else None
-     )
+  frequent_order_alert = (order_stream
+      .key_by(lambda order: order["customer_id"]) 
+      .window(TumblingEventTimeWindows.of(Time.minutes(1)))
+      .process(FrequentOrderProcessor(), output_type=Types.PICKLED_BYTE_ARRAY())
   )
 
   # Kết hợp tất cả các luồng cảnh báo
-  all_alerts = high_values_alert.union(large_quantity_alert, frequent_order_alert)
+  all_alerts = high_values_alert.union(large_quantity_alert).union(frequent_order_alert)
 
   # Gửi cảnh báo tới webhook
   if WEBHOOK_URL:
-    all_alerts.map(SendAlertToWebhook(WEBHOOK_URL), output_type=Types.PICKLED_BYTE_ARRAY()).print()  # In ra console để kiểm tra
+    all_alerts.map(SendAlertToWebhook(WEBHOOK_URL), output_type=Types.PICKLED_BYTE_ARRAY()).print()
   else:
     print("⚠️ WEBHOOK_URL not set. Skipping alert sending.")
     all_alerts.print()
   
   env.execute("Fraud Detection Order Stream")
 
-
-    
+if __name__ == "__main__": 
+  topic_src = "ecommere-cdc.public.orders"
+  group_id = "fraud-detection-order-group"
+  src_name = "Kafka Source - Orders"
+  fraud_detection_order(topic_src, group_id, src_name)
--- script/flink/utils/create_stream.py
+++ script/flink/utils/create_stream.py
@@ -2,47 +2,56 @@ from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.functions import MapFunction
 from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer
 from pyflink.common.serialization import SimpleStringSchema
+# from pyflink.datastream.formats.json import JsonRowDeserializationSchema
 from pyflink.common.typeinfo import Types
 from pyflink.common.watermark_strategy import WatermarkStrategy
 from pyflink.common import Duration
 from pyflink.datastream.window import TumblingEventTimeWindows
 from pyflink.common.time import Time
 import os
+import json
 
 JARS_PATH = f"{os.getcwd()}/config/kafka-connect/jars/"
-BOOTSTRAP_SERVERS = "broker:29092"
+BOOTSTRAP_SERVERS = "localhost:9092"
 
 def create_kafka_source(topic: str, group_id: str, source_name: str): 
-  env = StreamExecutionEnvironment.get_execution_environment()
-  env.set_parallelism(parallelism=2)
+  try: 
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(parallelism=2)
 
-  # Thêm các file JAR cần thiết để kết nối với Kafka
-  # Flink cần các dependency này để giao tiếp với Kafka cluster
-  jar_files = [
-    f"file://{JARS_PATH}/flink-connector-kafka-1.17.1.jar",
-    f"file://{JARS_PATH}/kafka-clients-3.4.0.jar",
-    f"file://{JARS_PATH}/flink-shaded-guava-30.1.1-jre-15.0.jar",
-  ]
-  env.add_jars(*jar_files)
+    # Thêm các file JAR cần thiết để kết nối với Kafka
+    # Flink cần các dependency này để giao tiếp với Kafka cluster
+    jar_files = [
+      f"file://{JARS_PATH}/flink-connector-kafka-1.17.1.jar",
+      f"file://{JARS_PATH}/kafka-clients-3.4.0.jar",
+      f"file://{JARS_PATH}/flink-shaded-guava-30.1.1-jre-15.0.jar",
+    ]
+    env.add_jars(*jar_files)
 
-  # kafka source
-  kafka_source = (
-  KafkaSource.builder() 
-    .set_bootstrap_servers(BOOTSTRAP_SERVERS) # Địa chỉ Kafka broker
-    .set_topics(topic)          # Tên topic để đọc dữ liệu
-    .set_group_id(group_id) # Consumer group ID
-    .set_starting_offsets(KafkaOffsetsInitializer.latest())  # Đọc từ message mới nhất
-    .set_value_only_deserializer(SimpleStringSchema())    # Deserializer cho dữ liệu
-    .build()
-  )
+    # kafka source
+    kafka_source = (
+    KafkaSource.builder() 
+      .set_bootstrap_servers(BOOTSTRAP_SERVERS) # Địa chỉ Kafka broker
+      .set_topics(topic)          # Tên topic để đọc dữ liệu
+      .set_group_id(group_id) # Consumer group ID
+      # .set_starting_offsets(KafkaOffsetsInitializer.latest())  # Đọc từ message mới nhất
+      .set_starting_offsets(KafkaOffsetsInitializer.latest())  # Đọc từ message mới nhất
+      .set_value_only_deserializer(SimpleStringSchema())    # Deserializer cho dữ liệu
+      .build()
+    )
 
-  raw_stream = env.from_source(
-    source=kafka_source, 
-    watermark_strategy=WatermarkStrategy.no_watermarks(), 
-    source_name=source_name
-  )
+    raw_stream = env.from_source(
+      source=kafka_source, 
+      watermark_strategy=WatermarkStrategy.no_watermarks(), 
+      source_name=source_name
+    )
 
-  return env, raw_stream
+    print("Kafka source created successfully.")
+
+    return env, raw_stream
+  except Exception as e: 
+    print("Error creating Kafka source:", e)
+    raise e
 
 
 def create_sink_kafka(): 
